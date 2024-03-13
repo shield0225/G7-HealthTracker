9d45dba664bd3dd2a4fdd6b66e02db919e098831
shield0225 <shieldsalcedo@yahoo.com>
2024-03-12 20:28:53 -0400

    Added Containers for Login and Registration

---
+++ src/components/Patient/Dashboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Dashboard.css";
+
+function Dashboard() {
+  return (
+    <>
+      <h1>Patient</h1>
+    </>
+  );
+}
+
+export default Dashboard;
---
+++ src/components/Patient/Personal.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Dashboard.css";
+
+function Personal() {
+  return (
+    <>
+      <h1>Patient</h1>
+    </>
+  );
+}
+
+export default Personal;
