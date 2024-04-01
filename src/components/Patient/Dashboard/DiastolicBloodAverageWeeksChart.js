8ae56d1e372acc93556a2beb459cf9b5dc65a22a
shield0225 <shieldsalcedo@yahoo.com>
2024-04-01 09:44:33 -0400

    Updating minor typo

--- src/components/Patient/Dashboard/DiastolicBloodAverageWeeksChart.js
+++ src/components/Patient/Dashboard/DiastolicBloodAverageWeeksChart.js
@@ -1,147 +1,170 @@
 import { Avatar, Grid, Stack, Typography, useTheme } from "@mui/material";
 import DashboardCard from "./DashboardCard";
-import { IconArrowDownRight, IconArrowUpLeft, IconStethoscope } from "@tabler/icons-react";
+import {
+  IconArrowDownRight,
+  IconArrowUpLeft,
+  IconStethoscope,
+} from "@tabler/icons-react";
 import Chart from "react-apexcharts";
 import { getAverageDailyInformationByWeeks } from "../../../helpers/chart-helper";
 import { patientData } from "../../../mock/patient-data";
 import { calculatePercentageHigher } from "../../../helpers/number-helper";
 
 const DiastolicBloodAverageWeeksChart = () => {
-    // chart color
-    const theme = useTheme();
-    const primary = theme.palette.primary.main;
-    const primarylight = '#ecf2ff';
-    const errorlight = '#fdede8';
+  // chart color
+  const theme = useTheme();
+  const primary = theme.palette.primary.main;
+  const primarylight = "#ecf2ff";
+  const errorlight = "#fdede8";
 
-    const averageResult = getAverageDailyInformationByWeeks(patientData.dailyInformation, 2, ['diastolicBloodPresure', 'systolicBloodPresure']);
-    const [twoWeeksAgo, oneWeekAgo, currentWeek] = averageResult.map(({ diastolicBloodPresure }) => parseInt(diastolicBloodPresure));
+  const averageResult = getAverageDailyInformationByWeeks(
+    patientData.dailyInformation,
+    2,
+    ["diastolicBloodPressure", "systolicBloodPressure"]
+  );
+  const [twoWeeksAgo, oneWeekAgo, currentWeek] = averageResult.map(
+    ({ diastolicBloodPressure }) => parseInt(diastolicBloodPressure)
+  );
 
-    const percentageHigherLastWeek = calculatePercentageHigher(currentWeek, oneWeekAgo);
+  const percentageHigherLastWeek = calculatePercentageHigher(
+    currentWeek,
+    oneWeekAgo
+  );
 
-    // chart
-    const optionscolumnchart = {
-        chart: {
-            type: 'donut',
-            fontFamily: "'Plus Jakarta Sans', sans-serif;",
-            foreColor: '#adb0bb',
-            toolbar: {
-                show: false,
-            },
-            height: 155,
+  // chart
+  const optionscolumnchart = {
+    chart: {
+      type: "donut",
+      fontFamily: "'Plus Jakarta Sans', sans-serif;",
+      foreColor: "#adb0bb",
+      toolbar: {
+        show: false,
+      },
+      height: 155,
+    },
+    labels: ["Current Week", "One Week Ago", "Two Weeks Ago"],
+    colors: [primary, primarylight, "#F9F9FD"],
+    plotOptions: {
+      pie: {
+        startAngle: 0,
+        endAngle: 360,
+        donut: {
+          size: "75%",
+          background: "transparent",
         },
-        labels: ['Current Week', 'One Week Ago', 'Two Weeks Ago'],
-        colors: [primary, primarylight, '#F9F9FD'],
-        plotOptions: {
-            pie: {
-                startAngle: 0,
-                endAngle: 360,
-                donut: {
-                    size: '75%',
-                    background: 'transparent',
-                },
-            },
+      },
+    },
+    tooltip: {
+      theme: theme.palette.mode === "dark" ? "dark" : "light",
+      fillSeriesColor: false,
+    },
+    stroke: {
+      show: false,
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    legend: {
+      show: false,
+    },
+    responsive: [
+      {
+        breakpoint: 991,
+        options: {
+          chart: {
+            width: 120,
+          },
         },
-        tooltip: {
-            theme: theme.palette.mode === 'dark' ? 'dark' : 'light',
-            fillSeriesColor: false,
-        },
-        stroke: {
-            show: false,
-        },
-        dataLabels: {
-            enabled: false,
-        },
-        legend: {
-            show: false,
-        },
-        responsive: [
-            {
-                breakpoint: 991,
-                options: {
-                    chart: {
-                        width: 120,
-                    },
-                },
-            },
-        ],
-    };
+      },
+    ],
+  };
 
-    const seriescolumnchart = [currentWeek, oneWeekAgo, twoWeeksAgo];
+  const seriescolumnchart = [currentWeek, oneWeekAgo, twoWeeksAgo];
 
-    const iconUpOrDown = () => {
-        if (percentageHigherLastWeek > 0) {
-            return <IconArrowUpLeft width={20} color="#FA896B" />;
-        }
-
-        if (percentageHigherLastWeek < 0) {
-            return <IconArrowDownRight width={20} color="#FA896B" />
-        }
-
-        return ""
+  const iconUpOrDown = () => {
+    if (percentageHigherLastWeek > 0) {
+      return <IconArrowUpLeft width={20} color="#FA896B" />;
     }
 
-    const showPercentage = () => {
-        if (percentageHigherLastWeek > 0) {
-            return `+${percentageHigherLastWeek}%`
-        }
-
-        if (percentageHigherLastWeek < 0) {
-            return `-${percentageHigherLastWeek}%`
-        }
-
-        return "0%"
+    if (percentageHigherLastWeek < 0) {
+      return <IconArrowDownRight width={20} color="#FA896B" />;
     }
 
-    return (
-        <DashboardCard title="Diastolic Average">
-            <Grid container spacing={3}>
-                {/* column */}
-                <Grid item xs={7} sm={7}>
-                    <Typography variant="h5" fontWeight="700">
-                        <IconStethoscope width={20} />  {currentWeek}
-                    </Typography>
-                    <Stack direction="row" spacing={1} mt={1} alignItems="center">
-                        <Avatar sx={{ bgcolor: errorlight, width: 27, height: 27 }}>
-                            {iconUpOrDown()}
-                        </Avatar>
-                        <Typography variant="subtitle2" fontWeight="600">
-                            {showPercentage()}
-                        </Typography>
-                        <Typography variant="subtitle2" color="textSecondary">
-                            last week
-                        </Typography>
-                    </Stack>
-                    <Stack spacing={3} mt={5} direction="row">
-                        <Stack direction="row" spacing={1} alignItems="center">
-                            <Avatar
-                                sx={{ width: 9, height: 9, bgcolor: primary, svg: { display: 'none' } }}
-                            ></Avatar>
-                            <Typography variant="subtitle2" color="textSecondary">
-                                Current
-                            </Typography>
-                        </Stack>
-                        <Stack direction="row" spacing={1} alignItems="center">
-                            <Avatar
-                                sx={{ width: 9, height: 9, bgcolor: primarylight, svg: { display: 'none' } }}
-                            ></Avatar>
-                            <Typography variant="subtitle2" color="textSecondary">
-                                Previous
-                            </Typography>
-                        </Stack>
-                    </Stack>
-                </Grid>
-                {/* column */}
-                <Grid item xs={5} sm={5}>
-                    <Chart
-                        options={optionscolumnchart}
-                        series={seriescolumnchart}
-                        type="donut"
-                        height="150px"
-                    />
-                </Grid>
-            </Grid>
-        </DashboardCard>
-    );
+    return "";
+  };
+
+  const showPercentage = () => {
+    if (percentageHigherLastWeek > 0) {
+      return `+${percentageHigherLastWeek}%`;
+    }
+
+    if (percentageHigherLastWeek < 0) {
+      return `-${percentageHigherLastWeek}%`;
+    }
+
+    return "0%";
+  };
+
+  return (
+    <DashboardCard title="Diastolic Average">
+      <Grid container spacing={3}>
+        {/* column */}
+        <Grid item xs={7} sm={7}>
+          <Typography variant="h5" fontWeight="700">
+            <IconStethoscope width={20} /> {currentWeek}
+          </Typography>
+          <Stack direction="row" spacing={1} mt={1} alignItems="center">
+            <Avatar sx={{ bgcolor: errorlight, width: 27, height: 27 }}>
+              {iconUpOrDown()}
+            </Avatar>
+            <Typography variant="subtitle2" fontWeight="600">
+              {showPercentage()}
+            </Typography>
+            <Typography variant="subtitle2" color="textSecondary">
+              last week
+            </Typography>
+          </Stack>
+          <Stack spacing={3} mt={5} direction="row">
+            <Stack direction="row" spacing={1} alignItems="center">
+              <Avatar
+                sx={{
+                  width: 9,
+                  height: 9,
+                  bgcolor: primary,
+                  svg: { display: "none" },
+                }}
+              ></Avatar>
+              <Typography variant="subtitle2" color="textSecondary">
+                Current
+              </Typography>
+            </Stack>
+            <Stack direction="row" spacing={1} alignItems="center">
+              <Avatar
+                sx={{
+                  width: 9,
+                  height: 9,
+                  bgcolor: primarylight,
+                  svg: { display: "none" },
+                }}
+              ></Avatar>
+              <Typography variant="subtitle2" color="textSecondary">
+                Previous
+              </Typography>
+            </Stack>
+          </Stack>
+        </Grid>
+        {/* column */}
+        <Grid item xs={5} sm={5}>
+          <Chart
+            options={optionscolumnchart}
+            series={seriescolumnchart}
+            type="donut"
+            height="150px"
+          />
+        </Grid>
+      </Grid>
+    </DashboardCard>
+  );
 };
 
 export default DiastolicBloodAverageWeeksChart;
--- src/components/Patient/Dashboard/SystolicBloodAverageChart.js
+++ src/components/Patient/Dashboard/SystolicBloodAverageChart.js
@@ -1,133 +1,175 @@
 import { Avatar, Fab, Stack, Typography, useTheme } from "@mui/material";
-import { IconArrowDownRight, IconArrowUpLeft, IconDropletPlus, IconStethoscope } from "@tabler/icons-react";
+import {
+  IconArrowDownRight,
+  IconArrowUpLeft,
+  IconDropletPlus,
+  IconStethoscope,
+} from "@tabler/icons-react";
 import DashboardCard from "./DashboardCard";
 import Chart from "react-apexcharts";
-import { filterGroupedDataByWeekDates, getHighestSystolicBloodPresureByDate } from "../../../helpers/chart-helper";
+import {
+  filterGroupedDataByWeekDates,
+  getHighestSystolicBloodPressureByDate,
+} from "../../../helpers/chart-helper";
 import { patientData } from "../../../mock/patient-data";
-import { generateWeekDates, getStartOfWeek, subtractWeeks } from "../../../helpers/date-helper";
+import {
+  generateWeekDates,
+  getStartOfWeek,
+  subtractWeeks,
+} from "../../../helpers/date-helper";
 import { calculatePercentageHigher } from "../../../helpers/number-helper";
 
 const SystolicBloodAverageChart = () => {
-    // chart color
-    const theme = useTheme();
-    const secondary = theme.palette.secondary.main;
-    const secondarylight = '#f5fcff';
-    const errorlight = '#fdede8';
+  // chart color
+  const theme = useTheme();
+  const secondary = theme.palette.secondary.main;
+  const secondarylight = "#f5fcff";
+  const errorlight = "#fdede8";
 
+  const highestSystolicBloodPressureByDate =
+    getHighestSystolicBloodPressureByDate(patientData.dailyInformation);
+  const startOfCurrentWeek = getStartOfWeek(new Date());
+  const startOfLastWeek = subtractWeeks(startOfCurrentWeek, 1);
+  const currentWeekDates = generateWeekDates(startOfCurrentWeek);
+  const lastWeekDates = generateWeekDates(new Date(startOfLastWeek));
 
-    const highestSystolicBloodPresureByDate = getHighestSystolicBloodPresureByDate(patientData.dailyInformation);
-    const startOfCurrentWeek = getStartOfWeek(new Date());
-    const startOfLastWeek = subtractWeeks(startOfCurrentWeek, 1);
-    const currentWeekDates = generateWeekDates(startOfCurrentWeek);
-    const lastWeekDates = generateWeekDates(new Date(startOfLastWeek));
-
-    const highestSystolicBloodPresureDataCurrentWeek = filterGroupedDataByWeekDates(highestSystolicBloodPresureByDate, currentWeekDates);
-    const highestSystolicBloodPresureDataLastWeek = filterGroupedDataByWeekDates(highestSystolicBloodPresureByDate, lastWeekDates);
-
-
-    const currentWeekAverage = highestSystolicBloodPresureDataCurrentWeek.reduce((acc, item) => acc + item, 0) / 7;
-    const lastWeekAverage = highestSystolicBloodPresureDataLastWeek.reduce((acc, item) => acc + item, 0) / 7;
-    const percentageHigherLastWeek = calculatePercentageHigher(currentWeekAverage, lastWeekAverage);
-
-    // chart
-    const optionscolumnchart = {
-        chart: {
-            type: 'area',
-            fontFamily: "'Plus Jakarta Sans', sans-serif;",
-            foreColor: '#adb0bb',
-            toolbar: {
-                show: false,
-            },
-            height: 60,
-            sparkline: {
-                enabled: true,
-            },
-            group: 'sparklines',
-        },
-        stroke: {
-            curve: 'smooth',
-            width: 2,
-        },
-        fill: {
-            colors: [secondarylight],
-            type: 'solid',
-            opacity: 0.05,
-        },
-        markers: {
-            size: 0,
-        },
-        tooltip: {
-            theme: theme.palette.mode === 'dark' ? 'dark' : 'light',
-        },
-        labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
-    };
-    const seriescolumnchart = [
-        {
-            name: 'Current Week',
-            color: secondary,
-            data: highestSystolicBloodPresureDataCurrentWeek,
-        },
-        {
-            name: 'Last Week',
-            color: '#FDBB3A',
-            data: highestSystolicBloodPresureDataLastWeek,
-        },
-    ];
-
-    const iconUpOrDown = () => {
-        if (percentageHigherLastWeek > 0) {
-            return <IconArrowUpLeft width={20} color="#FA896B" />;
-        }
-
-        if (percentageHigherLastWeek < 0) {
-            return <IconArrowDownRight width={20} color="#FA896B" />
-        }
-
-        return ""
-    }
-
-    const showPercentage = () => {
-        if (percentageHigherLastWeek > 0) {
-            return `+${percentageHigherLastWeek}%`
-        }
-
-        if (percentageHigherLastWeek < 0) {
-            return `-${percentageHigherLastWeek}%`
-        }
-
-        return "0%"
-    }
-
-    return (
-        <DashboardCard
-            title="Systolic Average"
-            action={
-                <Fab color="secondary" size="medium" sx={{ color: '#ffffff' }}>
-                    <IconDropletPlus width={24} />
-                </Fab>
-            }
-            footer={
-                <Chart options={optionscolumnchart} series={seriescolumnchart} type="area" height="60px" />
-            }
-        >
-            <>
-                <Typography variant="h5" fontWeight="700" mt="-20px">
-                    <IconStethoscope width={20} />  {parseInt(currentWeekAverage)}
-                </Typography>
-                <Stack direction="row" spacing={1} my={1} alignItems="center">
-                    <Avatar sx={{ bgcolor: errorlight, width: 27, height: 27 }}>
-                        {iconUpOrDown()}
-                    </Avatar>
-                    <Typography variant="subtitle2" fontWeight="600">
-                        {showPercentage()}
-                    </Typography>
-                    <Typography variant="subtitle2" color="textSecondary">
-                        last week
-                    </Typography>
-                </Stack>
-            </>
-        </DashboardCard>
+  const highestSystolicBloodPressureDataCurrentWeek =
+    filterGroupedDataByWeekDates(
+      highestSystolicBloodPressureByDate,
+      currentWeekDates
     );
+  const highestSystolicBloodPressureDataLastWeek = filterGroupedDataByWeekDates(
+    highestSystolicBloodPressureByDate,
+    lastWeekDates
+  );
+
+  const currentWeekAverage =
+    highestSystolicBloodPressureDataCurrentWeek.reduce(
+      (acc, item) => acc + item,
+      0
+    ) / 7;
+  const lastWeekAverage =
+    highestSystolicBloodPressureDataLastWeek.reduce(
+      (acc, item) => acc + item,
+      0
+    ) / 7;
+  const percentageHigherLastWeek = calculatePercentageHigher(
+    currentWeekAverage,
+    lastWeekAverage
+  );
+
+  // chart
+  const optionscolumnchart = {
+    chart: {
+      type: "area",
+      fontFamily: "'Plus Jakarta Sans', sans-serif;",
+      foreColor: "#adb0bb",
+      toolbar: {
+        show: false,
+      },
+      height: 60,
+      sparkline: {
+        enabled: true,
+      },
+      group: "sparklines",
+    },
+    stroke: {
+      curve: "smooth",
+      width: 2,
+    },
+    fill: {
+      colors: [secondarylight],
+      type: "solid",
+      opacity: 0.05,
+    },
+    markers: {
+      size: 0,
+    },
+    tooltip: {
+      theme: theme.palette.mode === "dark" ? "dark" : "light",
+    },
+    labels: [
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+      "Sunday",
+    ],
+  };
+  const seriescolumnchart = [
+    {
+      name: "Current Week",
+      color: secondary,
+      data: highestSystolicBloodPressureDataCurrentWeek,
+    },
+    {
+      name: "Last Week",
+      color: "#FDBB3A",
+      data: highestSystolicBloodPressureDataLastWeek,
+    },
+  ];
+
+  const iconUpOrDown = () => {
+    if (percentageHigherLastWeek > 0) {
+      return <IconArrowUpLeft width={20} color="#FA896B" />;
+    }
+
+    if (percentageHigherLastWeek < 0) {
+      return <IconArrowDownRight width={20} color="#FA896B" />;
+    }
+
+    return "";
+  };
+
+  const showPercentage = () => {
+    if (percentageHigherLastWeek > 0) {
+      return `+${percentageHigherLastWeek}%`;
+    }
+
+    if (percentageHigherLastWeek < 0) {
+      return `-${percentageHigherLastWeek}%`;
+    }
+
+    return "0%";
+  };
+
+  return (
+    <DashboardCard
+      title="Systolic Average"
+      action={
+        <Fab color="secondary" size="medium" sx={{ color: "#ffffff" }}>
+          <IconDropletPlus width={24} />
+        </Fab>
+      }
+      footer={
+        <Chart
+          options={optionscolumnchart}
+          series={seriescolumnchart}
+          type="area"
+          height="60px"
+        />
+      }
+    >
+      <>
+        <Typography variant="h5" fontWeight="700" mt="-20px">
+          <IconStethoscope width={20} /> {parseInt(currentWeekAverage)}
+        </Typography>
+        <Stack direction="row" spacing={1} my={1} alignItems="center">
+          <Avatar sx={{ bgcolor: errorlight, width: 27, height: 27 }}>
+            {iconUpOrDown()}
+          </Avatar>
+          <Typography variant="subtitle2" fontWeight="600">
+            {showPercentage()}
+          </Typography>
+          <Typography variant="subtitle2" color="textSecondary">
+            last week
+          </Typography>
+        </Stack>
+      </>
+    </DashboardCard>
+  );
 };
 
 export default SystolicBloodAverageChart;
