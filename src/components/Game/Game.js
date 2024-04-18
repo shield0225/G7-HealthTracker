6fc3a8c4b7d9ddecc9a489d126000d5d208a6a0f
shield0225 <shieldsalcedo@yahoo.com>
2024-04-18 12:43:22 -0400

    attempting to fix #36

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -5,30 +5,28 @@ import NavBar from "../NavBar";
 import Footer from "../Footer";
 
 function Game() {
-    const { unityProvider, loadingProgression, isLoaded } = useUnityContext({
-        loaderUrl: "build/KeepMovingBuild.loader.js",
-        dataUrl: "build/KeepMovingBuild.data",
-        frameworkUrl: "build/KeepMovingBuild.framework.js",
-        codeUrl: "build/KeepMovingBuild.wasm"
-    });
+  const { unityProvider, loadingProgression, isLoaded } = useUnityContext({
+    loaderUrl: "build/KeepMovingBuild.loader.js",
+    dataUrl: "build/KeepMovingBuild.data",
+    frameworkUrl: "build/KeepMovingBuild.framework.js",
+    codeUrl: "build/KeepMovingBuild.wasm",
+  });
 
-    return (
-        <Container>
-            <NavBar />
-            <br />
-            <Fragment>
-                {!isLoaded && (
-                    <ProgressBar animated now={loadingProgression * 100}/>
-                )}
-                <Unity
-                    unityProvider={unityProvider}
-                    style={{ visibility: isLoaded ? "visible" : "hidden" }}
-                />
-            </Fragment>
-            <br />
-            <Footer />
-        </Container>
-    )
+  return (
+    <Container>
+      <NavBar />
+      <br />
+      <Fragment>
+        {!isLoaded && <ProgressBar animated now={loadingProgression * 100} />}
+        <Unity
+          unityProvider={unityProvider}
+          style={{ visibility: isLoaded ? "visible" : "hidden" }}
+        />
+      </Fragment>
+      <br />
+      <Footer />
+    </Container>
+  );
 }
 
 export default Game;
